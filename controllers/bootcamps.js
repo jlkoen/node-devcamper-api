8722059d8382622d6ae8054f753dca16d2dc1bb4
John Koen <jkoen@smartronix.com>
2022-01-05 16:39:12 -0500

    Added bootcamps POST method

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const database = require('mime-db');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc      Get all bootcamps
@@ -19,9 +20,13 @@ exports.getBootcamp = (req, res, next) => {
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
-exports.createBootcamp = (req, res, next) => {
-  console.log(req.body);
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 };
 
 // @desc      Update bootcamp
